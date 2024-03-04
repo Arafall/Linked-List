f23c2cd4418a4f8de6fbf0b1fa26c52802b28ba8
Daniel Wade <danielwade101@gmail.com>
2024-03-04 16:06:42 -0600

    Initial commit.

---
+++ src/edu/otc/Main.java
@@ -0,0 +1,12 @@
+package edu.otc;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList ll = new LinkedList();
+        ll.add(15);
+        ll.add(30);
+        ll.add(45);
+        ll.printList();
+    }
+}
